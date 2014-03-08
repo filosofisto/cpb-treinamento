ac0dfac9288d3be877662f6c3acd42226d577b60
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2014-03-08 15:15:17 -0300

    Pastas vazias
    Adaptacao para a estrutua Maven

--- workspace-treinamento-cpb/operators/src/main/java/com/javabasico/Main.java
+++ workspace-treinamento-cpb/operators/src/main/java/com/javabasico/Main.java
@@ -2,6 +2,8 @@ package com.javabasico;
 
 public class Main {
 
+	private static char variavel;
+	
 	public static void main(String[] args) {
 		//-----------------------------------
 		//Atribuicao: =
@@ -67,6 +69,30 @@ public class Main {
 			System.out.println("Sempre imprimira esta mensagem tambem");
 		}
 		
+		variavel = 'B';
+		if (1 == 1 || alteraVariavelERetornaTrue()) {
+			System.out.printf("Gostaria que variavel fosse igual a 'A', mas o valor dela eh %s\n", variavel);
+		}
+		
+		variavel = 'B';
+		if (1 == 1 | alteraVariavelERetornaTrue()) {
+			System.out.printf("Gostaria que variavel fosse igual a 'A', e o valor dela eh %s\n", variavel);
+		}
+		
+		variavel = 'B';
+		if (1 == 0 && alteraVariavelERetornaTrue()) {
+			//Nunca entrara aqui
+		} else {
+			System.out.printf("Gostaria que variavel fosse igual a 'A', mas o valor dela eh %s\n", variavel);
+		}
+		
+		variavel = 'B';
+		if (1 == 0 & alteraVariavelERetornaTrue()) {
+			//Nunca entrara aqui
+		} else {
+			System.out.printf("Gostaria que variavel fosse igual a 'A', e o valor dela eh %s\n", variavel);
+		}
+		
 		//-----------------------------------
 		//Operador ~ (inverte os bytes)
 		//-----------------------------------
@@ -79,5 +105,55 @@ public class Main {
 		//-----------------------------------
 		System.out.println((a > b) ? "A maior que B" : "A nao eh maior que B");
 		System.out.println((a > b) ? "A maior que B" : (b > a) ? "B maior que A" : "Ambos sao iguais");
+		
+		//-----------------------------------
+		//Incremento e Decremento
+		//-----------------------------------
+		a = 10;
+		System.out.printf("Valor original de A: %d. \nApos o a++ o valor novo eh: %d, com ++a sera %d\n", a, a++, ++a);
+		System.out.printf("Decrementando o valor de A teremos: %d\n", --a);
+		
+		//-----------------------------------
+		//Matematicos
+		//-----------------------------------
+		a = 4;
+		a = a * 3;
+		System.out.printf("a * 3: %d\n", a);
+		a = 4;
+		a *= 3;
+		System.out.printf("a *= 3: %d\n", a);
+		
+		if (a % 2 == 0) {
+			System.out.printf("%d eh par\n", a);
+		} else {
+			System.out.printf("%d eh impar\n", a);
+		}
+
+		//-----------------------------------
+		//Binarios
+		//-----------------------------------
+		byte b1 = 11; // 0000 1011
+		if ((b1 & 1) == 1) { // 0000 0001
+			System.out.println("O bit 1 esta ON");
+		}
+		if ((b1 & 2) == 2) { // 0000 0010
+			System.out.println("O bit 2 esta ON");
+		}
+		if ((b1 & 4) == 4) { // 0000 0100
+			System.out.println("O bit 3 esta ON");
+		}
+		if ((b1 & 8) == 8) { // 0000 1000
+			System.out.println("O bit 4 esta ON");
+		}
+		
+		b1 = 1;
+		b1 <<= 2; //Multiplicou por 4
+		System.out.printf("%d 2 bits a esquerda (shift): %d", 1, b1);
+	}
+	
+	private static boolean alteraVariavelERetornaTrue() {
+		variavel = 'A';
+		
+		return true;
 	}
 }
