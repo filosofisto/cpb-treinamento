25ff804c480e87cf0c560ef5513b3e34707edd8d
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2014-03-11 19:54:43 -0300

    Comentario hello-world
    .

--- workspace-treinamento-cpb/hello-world/src/main/java/com/javabasico/Main.java
+++ workspace-treinamento-cpb/hello-world/src/main/java/com/javabasico/Main.java
@@ -2,6 +2,11 @@ package com.javabasico;
 
 public class Main {
 
+	/**
+	 * Metodo principal acionado pela JVM.
+	 * 
+	 * @param args Lista de argumentos no console
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
